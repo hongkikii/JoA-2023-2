40f4547c55133428745574acef1a62013ecec788
JongHyeon Choi <choijonghyen@gmail.com>
2024-01-31 12:31:55 +0900

    refactor: DTO 가이드 라인에 맞게 수정

--- src/main/java/com/mjuAppSW/joA/domain/roomInMember/dto/request/VoteRequest.java
+++ src/main/java/com/mjuAppSW/joA/domain/roomInMember/dto/request/VoteRequest.java
@@ -3,14 +3,18 @@ package com.mjuAppSW.joA.domain.roomInMember.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @Schema(description = "채팅방 연장 투표 저장 및 상대방 투표 유무 확인 Request")
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class VoteRequest {
     @NotNull
-    private Long roomId;
+    private final Long roomId;
     @NotNull
-    private Long memberId;
+    private final Long memberId;
     @NotNull
-    private String result;
+    private final String result;
 }
