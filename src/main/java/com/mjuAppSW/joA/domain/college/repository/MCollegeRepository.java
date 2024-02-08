23814eca76160e0185c95ed14d2ccf2e4a40cfc9
hongkikii <hyangmeeh@icloud.com>
2024-02-08 12:24:46 +0900

    style: 코드 라인 수정

--- src/main/java/com/mjuAppSW/joA/domain/college/repository/MCollegeRepository.java
+++ src/main/java/com/mjuAppSW/joA/domain/college/repository/MCollegeRepository.java
@@ -4,7 +4,10 @@ import com.mjuAppSW.joA.domain.college.MCollege;
 import java.util.Optional;
 
 public interface MCollegeRepository {
+
     void save(MCollege mCollege);
+
     Optional<MCollege> findById(Long collegeId);
+
     Optional<MCollege> findByDomain(String domain);
 }
