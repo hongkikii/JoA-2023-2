259e2bcc5f618e4b26a1e525e159bf2c37013a79
hongkikii <hyangmeeh@icloud.com>
2024-02-16 14:19:03 +0900

    style: 불필요 import 삭제

--- src/main/java/com/mjuAppSW/joA/JProjectApplication.java
+++ src/main/java/com/mjuAppSW/joA/JProjectApplication.java
@@ -2,8 +2,6 @@ package com.mjuAppSW.joA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
