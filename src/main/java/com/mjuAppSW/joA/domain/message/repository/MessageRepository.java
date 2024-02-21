8f2983927d4e1b867f3ea81e26d16686be983565
JongHyeon Choi <choijonghyen@gmail.com>
2024-02-21 17:11:19 +0900

    refactor: Repository 메소드 수정 및 사용하지 않는 메소드 삭제

--- src/main/java/com/mjuAppSW/joA/domain/message/repository/MessageRepository.java
+++ src/main/java/com/mjuAppSW/joA/domain/message/repository/MessageRepository.java
@@ -1,15 +1,13 @@
 package com.mjuAppSW.joA.domain.message.repository;
 
-import com.mjuAppSW.joA.domain.member.entity.Member;
 import com.mjuAppSW.joA.domain.message.dto.vo.CurrentMessageVO;
 import com.mjuAppSW.joA.domain.message.entity.Message;
 import com.mjuAppSW.joA.domain.room.entity.Room;
-import jakarta.transaction.Transactional;
+import com.mjuAppSW.joA.domain.member.entity.Member;
+
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,21 +18,14 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     @Query("SELECT m FROM Message m Where m.room = :room")
     List<Message> findByRoom(@Param("room") Room room);
 
-    @Query("SELECT m FROM Message m WHERE m.room = :room AND m.isChecked = '1' AND m.member <> :member")
-    List<Message> findMessage(@Param("room") Room room, @Param("member") Member member);
+    @Query("SELECT m FROM Message m WHERE m.room = :room AND m.member <> :member AND m.isChecked = :isChecked")
+    List<Message> findOpponentByRoomAndMemberAndIsChecked(@Param("room") Room room, @Param("member") Member member, @Param("isChecked") String isChecked);
 
-
-    @Query("SELECT COUNT(m) FROM Message m WHERE m.room = :room AND m.member = :member AND m.isChecked = '1'")
-    Integer countUnCheckedMessage(@Param("room") Room room, @Param("member") Member member);
-
+    @Query("SELECT COUNT(m) FROM Message m WHERE m.room = :room AND m.member = :member AND m.isChecked = :isChecked")
+    Integer countUnCheckedMessagesByRoomAndMemberAndIsChecked(@Param("room") Room room, @Param("member") Member member, @Param("isChecked") String isChecked);
 
     @Query("SELECT m.content AS content, m.time AS time FROM Message m WHERE m.room = :room " +
-            "AND m.content IS NULL OR m.content IS NOT NULL " +
-            "AND (m.time IS NULL OR m.time = (SELECT MAX(mes2.time) FROM Message mes2 WHERE mes2.room = :room))")
-    Optional<CurrentMessageVO> getCurrentMessageAndTime(@Param("room") Room room);
-
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM Message m WHERE m.room = :room")
-    void deleteByRoom(@Param("room") Room room);
+        "AND (m.content IS NULL OR m.content IS NOT NULL) " +
+        "ORDER BY m.time DESC")
+    List<CurrentMessageVO> findCurrentMessageByRoom(@Param("room") Room room);
 }
--- src/main/java/com/mjuAppSW/joA/domain/roomInMember/repository/RoomInMemberRepository.java
+++ src/main/java/com/mjuAppSW/joA/domain/roomInMember/repository/RoomInMemberRepository.java
@@ -1,19 +1,16 @@
 package com.mjuAppSW.joA.domain.roomInMember.repository;
 
-import com.mjuAppSW.joA.domain.member.entity.Member;
-import com.mjuAppSW.joA.domain.member.vo.UserInfoVO;
 import com.mjuAppSW.joA.domain.room.entity.Room;
 import com.mjuAppSW.joA.domain.roomInMember.entity.RoomInMember;
+import com.mjuAppSW.joA.domain.member.entity.Member;
+import com.mjuAppSW.joA.domain.member.vo.UserInfoVO;
 import com.mjuAppSW.joA.domain.roomInMember.vo.RoomInfoExceptMessageVO;
 import com.mjuAppSW.joA.domain.roomInMember.vo.RoomInfoIncludeMessageVO;
-
-import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -24,11 +21,11 @@ public interface RoomInMemberRepository extends JpaRepository<RoomInMember, Long
     @Query("SELECT rim FROM RoomInMember rim WHERE rim.room = :room AND rim.member = :member")
     Optional<RoomInMember> findByRoomAndMember(@Param("room") Room room, @Param("member") Member member);
 
-    @Query("SELECT rim FROM RoomInMember rim WHERE rim.member = :member AND rim.expired = '1'")
-    List<RoomInMember> findByAllMember(@Param("member") Member member);
+    @Query("SELECT rim FROM RoomInMember rim WHERE rim.member = :member AND rim.expired = :expired")
+    List<RoomInMember> findByMemberAndExpired(@Param("member") Member member, @Param("expired") String expired);
 
     @Query("SELECT rim FROM RoomInMember rim WHERE rim.room = :room")
-    List<RoomInMember> findByAllRoom(@Param("room") Room room);
+    List<RoomInMember> findByRoom(@Param("room") Room room);
 
     @Query("SELECT rm FROM RoomInMember rm " +
         "WHERE rm.room.id IN (SELECT r.id FROM Room r " +
@@ -41,28 +38,20 @@ public interface RoomInMemberRepository extends JpaRepository<RoomInMember, Long
             "LEFT JOIN Message mes ON rim.member = mes.member AND rim.room = mes.room " +
             "WHERE rim.member = :member AND rim.room = :room " +
             "ORDER BY mes.time DESC")
-    List<RoomInfoIncludeMessageVO> findRoomInfoIncludeMessage(@Param("room") Room room, @Param("member") Member member);
+    List<RoomInfoIncludeMessageVO> findIncludeMessageByRoomAndMember(@Param("room") Room room, @Param("member") Member member);
 
     @Query("SELECT rim.room AS room, rim.room.date AS date, rim.member.name AS name, rim.member.urlCode AS urlCode " +
         "From RoomInMember rim WHERE rim.member = :member AND rim.room = :room")
-    Optional<RoomInfoExceptMessageVO> findRoomInfoExceptMessageByRoomAndMember(@Param("room") Room room, @Param("member") Member member);
-
-    @Query("SELECT rim FROM RoomInMember rim Where rim.room = :room")
-    List<RoomInMember> findAllRoom(@Param("room") Room room);
+    Optional<RoomInfoExceptMessageVO> findExceptMessageByRoomAndMember(@Param("room") Room room, @Param("member") Member member);
 
     @Query("SELECT rim FROM RoomInMember rim Where rim.room = :room AND rim.member <> :member")
     Optional<RoomInMember> findOpponentByRoomAndMember(@Param("room") Room room, @Param("member") Member member);
 
     @Query("SELECT rim FROM RoomInMember rim Where rim.room = :room AND rim.member <> :member AND rim.expired = :expired")
-    Optional<RoomInMember> checkOpponentExpired(@Param("room") Room room, @Param("member") Member member, @Param("expired") String expired);
+    Optional<RoomInMember> findOpponentByRoomAndMemberAndExpired(@Param("room") Room room, @Param("member") Member member, @Param("expired") String expired);
 
     @Query("SELECT rim.room.id AS roomId ,rim.member.id AS memberId, rim.member.name AS name, rim.member.urlCode AS urlCode, rim.member.bio AS bio " +
         "FROM RoomInMember rim WHERE rim.room = :room AND rim.member <> :member")
     Optional<UserInfoVO> findOpponentUserInfoByRoomAndMember(@Param("room") Room room, @Param("member") Member member);
-
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM RoomInMember rim WHERE rim.room = :room")
-    void deleteByRoom(@Param("room") Room room);
 }
 
