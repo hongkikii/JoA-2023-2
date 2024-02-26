6c4d7dcd8350928ea90e4d6934f04197dbf3c1bd
JongHyeon Choi <choijonghyen@gmail.com>
2024-02-26 12:20:17 +0900

    refactor: @Pathvariable -> @RequestParam으로 변경

--- src/main/java/com/mjuAppSW/joA/domain/roomInMember/controller/RoomInMemberApiController.java
+++ src/main/java/com/mjuAppSW/joA/domain/roomInMember/controller/RoomInMemberApiController.java
@@ -36,8 +36,8 @@ public class RoomInMemberApiController {
         @ApiResponse(responseCode = "404-2", description = "RIM001: 사용자와 연결된 채팅방을 찾을 수 없습니다.", content = @Content(schema = @Schema(hidden = true))),
         @ApiResponse(responseCode = "500", description = "MG003: 메시지 복호화에 실패했습니다.", content = @Content(schema = @Schema(hidden = true))),
     })
-    @GetMapping("/{memberId}")
-    public ResponseEntity<SuccessResponse<RoomListResponse>> getChattingRoomListPage(@PathVariable("memberId") Long memberId){
+    @GetMapping
+    public ResponseEntity<SuccessResponse<RoomListResponse>> getChattingRoomListPage(@RequestParam("memberId") Long memberId){
         return SuccessResponse.of(roomInMemberService.getChattingRoomListPage(memberId))
             .asHttp(HttpStatus.OK);
     }
