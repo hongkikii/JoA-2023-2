c4e315f14c39bcffd07787ce41eb22a11043b047
hongkikii <hyangmeeh@icloud.com>
2024-03-05 13:16:22 +0900

    refactor: 동일 차단 존재 유무 확인 쿼리 락 설정

--- src/main/java/com/mjuAppSW/joA/geography/block/repository/BlockJpaRepository.java
+++ src/main/java/com/mjuAppSW/joA/geography/block/repository/BlockJpaRepository.java
@@ -1,14 +1,17 @@
 package com.mjuAppSW.joA.geography.block.repository;
 
 import com.mjuAppSW.joA.geography.block.entity.Block;
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface BlockJpaRepository extends JpaRepository<Block, Long> {
 
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT b FROM Block b WHERE b.blocker.id = :blockerId AND b.blocked.id = :blockedId")
     Optional<Block> findEqualBy(@Param("blockerId") Long blockerId, @Param("blockedId") Long blockedId);
 
